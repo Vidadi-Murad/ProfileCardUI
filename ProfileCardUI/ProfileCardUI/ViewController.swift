c8327364e815c686ef3144c06791c464db336ef4
VidadiMurad <vidad0murad@gmail.com>
2026-04-24 20:25:54 +0400

    First Commit

---
+++ ProfileCardUI/ProfileCardUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProfileCardUI
+//
+//  Created by Vidadi Muradzada on 24.04.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
